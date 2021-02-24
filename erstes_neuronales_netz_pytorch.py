e6cb8bbb8497f087751f2973a9312accbfdc4932
DominikBurkert <dominik.al.burkert@gmail.com>
2021-02-24 12:59:58 +0100

    commit - all imports

--- erstes_neuronales_netz_pytorch.py
+++ erstes_neuronales_netz_pytorch.py
@@ -1,4 +1,15 @@
+#imports
+
+import torch
+import torchvision
+import torch.nn as nn # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
+import torch.optim as optim # For all Optimization algorithms, SGD, Adam, etc.
+import torch.nn.functional as F # All functions that don't have any parameters
+from torch.utils.data import DataLoader # Gives easier dataset management and creates mini batches
+import torchvision.datasets as datasets # Has standard datasets we can import in a nice way
+import torchvision.transforms as transforms # Transformations we can perform on our dataset
+
+#Create Fully Connected Network
+
 
 print("hallo")
-
-print("halllo2")
